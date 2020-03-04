68cb5077d7ddc8037f753d5d2eee34176bde8ef5
Enrico Buratto <enrico.buratto.1@studenti.unipd.it>
2020-03-04 19:59:09 +0100

    Added github action for autodeploy

--- views/template/header.php
+++ views/template/header.php
@@ -4,7 +4,7 @@
     <title><?=$page->title?></title>
     <meta charset="utf8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="styles/stylesheet.css">
+    <link rel="stylesheet" href="styles/css/stylesheet.css">
 </head>
 
 <body>
