080bd61948d944d725610b63b2bc8606f59cb52e
Enrico Buratto <enrico.buratto.1@studenti.unipd.it>
2020-03-05 18:50:53 +0100

    Aggiunte altre pagine, finito header

--- contacts.php
+++ contacts.php
@@ -8,17 +8,17 @@ require_once("includes/resources.php");
 
 $page = (object) array(
 
-    "title" => "Homepage | Enrico Buratto",
-    "desc" => "Homepage",
-    "keywords" => "",
-    "name" => "index",
+    "title" => "Contatti | Enrico Buratto",
+    "desc" => "Contatti",
+    "keywords" => "contacts",
+    "name" => "contacts",
     //"breadcrumb" => array($lang->aboutTitle => "about.php")
 );
 
 
 require_once("views/template/header.php");
 
-require_once("views/index.php");
+require_once("views/contacts.php");
 
 require_once("views/template/footer.php");
 
